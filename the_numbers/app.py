3a0b5b6f265dd9327bedf11f2606ccf510837c68
Fabrício <fab.brito94@gmail.com>
2020-09-01 15:17:21 -0300

    fix: all letters to upercase

--- the_numbers/app.py
+++ the_numbers/app.py
@@ -3,7 +3,7 @@
 v1 = [16, 9, 3, 15, 3, 20, 6]
 v2 = [20, 8, 5, 14, 21, 13, 2, 5, 18, 19, 13, 1, 19, 15, 14]
 
-flag = ''.join([chr(x+ord('a')-1) for x in v1]) + \
-    '{' + ''.join([chr(x+ord('a')-1) for x in v2]) + '}'
+flag = ''.join([chr(x+ord('A')-1) for x in v1]) + \
+    '{' + ''.join([chr(x+ord('A')-1) for x in v2]) + '}'
 
 print(flag)
